bdfd10d95cb5f24bf1da54b7ca533ffbf9b2dea6
Hasan Tareq <hsntareq@gmail.com>
2021-04-10 15:29:45 +0600

    login/register templat with route functional

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-white h-full sm:bg-gray-100 flex justify-center">
-    <div class="container mt-2 sm:mt-10 flex flex-col items-center">
+  <div class="bg-white sm:bg-gray-100 h-full flex justify-center">
+    <div class="container sm:w-400p mt-2 sm:mt-10 flex flex-col items-center">
       <div class="text-3xl text-blue-700 font-bold mb-10">
         <span>Laravello</span>
       </div>
       <div class="w-full sm:shadow-xl sm:bg-white sm:py-8 sm:px-12">
         <div class="w-full text-center text-gray-600 font-bold mb-8">
-          Log in to Laravello
+          Signup for your account
         </div>
         <form>
           <div class="w-full mb-4">
@@ -14,7 +14,13 @@
               type="text"
               placeholder="Enter email"
               class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
-              autocomplete="off"
+            />
+          </div>
+          <div class="w-full mb-4">
+            <input
+              type="text"
+              placeholder="Enter full name"
+              class="rounded-sm px-4 py-2 outline-none focus:outline-none border-gray-400 bg-gray-100 border-solid border-2 w-full text-sm"
             />
           </div>
           <div class="w-full mb-4">
@@ -29,20 +35,21 @@
               type="submit"
               class="rounded-md px-4 py-2 text-sm bg-green-500 font-bold outline-none focus:outline-none hover:opacity-75 w-full text-white disabled:opacity-25"
             >
-              Login
+              Signup
             </button>
           </div>
         </form>
         <div class="bg-gray-400 h-px w-full mb-6"></div>
         <div class="text-center text-sm">
-          <router-link :to="{ name: 'register' }" class="text-blue-600 hover:underline"
-            >Sign up for an account
+          <router-link :to="{ name: 'login' }" class="text-blue-600 hover:underline">
+            Already have an account? Log In
           </router-link>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <style scoped>
 .container {
   width: 300px;
